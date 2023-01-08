d1c4a85c1c9570d688dcabe6017658ee0f000005
Shashank Atreya <satreya@liquipedia.net>
2023-01-09 03:18:37 +0530

    day 6

---
+++ day6.py
@@ -0,0 +1,45 @@
+with open('day6input.txt') as f:
+    fishes = f.read().splitlines()
+
+fishes = list(map(int, fishes[0].split(',')))
+
+# Part 1: 380243
+# Part 2: 1708791884591
+
+def solve(forPart1 = False):
+    if forPart1:
+        days = 80
+    else:
+        days = 256
+    currentState = {
+        0: fishes.count(0),
+        1: fishes.count(1),
+        2: fishes.count(2),
+        3: fishes.count(3),
+        4: fishes.count(4),
+        5: fishes.count(5),
+        6: fishes.count(6),
+        7: fishes.count(7),
+        8: fishes.count(8),
+    }
+
+    for i in range(days):
+        newState = {
+            0: currentState[1],
+            1: currentState[2],
+            2: currentState[3],
+            3: currentState[4],
+            4: currentState[5],
+            5: currentState[6],
+            6: currentState[7] + currentState[0],
+            7: currentState[8],
+            8: currentState[0],
+        }
+        currentState = newState
+    total = 0
+    for fish in currentState:
+        total += currentState[fish]
+    return total
+
+print("Part 1:", solve(True))
+print("Part 2:", solve())
