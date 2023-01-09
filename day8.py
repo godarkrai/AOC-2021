7ea696779fc56b62a327ba45ff7020730d56d167
Shashank Atreya <satreya@liquipedia.net>
2023-01-09 19:31:16 +0530

    minor changes

--- day8.py
+++ day8.py
@@ -1,6 +1,15 @@
 signals = open('day8input.txt').read().splitlines()
 
-def getNumberFromWord(word, d):
+'''
+The letter 8:
+    1
+2       3
+    4
+5       6
+    7
+'''
+
+def getNumberFromWord(word, dictionary):
     if len(word) == 2:
         return 1
     if len(word) == 3:
@@ -10,15 +19,21 @@ def getNumberFromWord(word, d):
     if len(word) == 7:
         return 8
     if len(word) == 5:
-        s = set(word)
-        if   len(s & d[2]) == 2: return 3
-        elif len(s & d[4]) == 2: return 2
-        else:                    return 5
-    else: # l == 6
-        s = set(word)
-        if   len(s & d[2]) == 1: return 6
-        elif len(s & d[4]) == 4: return 9
-        else:                    return 0
+        # Letter 3 = All letters of 7 are in 3 so and 3 is the only length 5 word to have it
+        hasAll = all([char in word for char in dictionary[7]])
+        if hasAll:
+            return 3
+        elif len(set(word) & set(dictionary[4])) == 2:
+            return 2
+        else:
+            return 5
+    elif len(word) == 6: #either 6 or 9
+        if len(set(word) & set(dictionary[1])) == 1:
+            return 6
+        elif len(set(word) & set(dictionary[4])) == 4:
+            return 9
+        else:
+            return 0
 '''
 fdgacbe cefdb cefbgd gcbe: 8394 c
 fcgedb cgb dgebacf gc: 9781 c
@@ -35,14 +50,20 @@ count = 0
 total = 0
 for signal in signals:
     decoded, code = signal.split(' | ')
-    d = {
-        l: set(s)
-        for s in decoded.split()
-        if (l := len(s)) in (2, 4)
-    }
+    wordDictionary = {}
+    # First make this dictionary because we are making other words from it
+    for word in decoded.split():
+        if len(word) == 2:
+            wordDictionary[1] = word
+        if len(word) == 3:
+            wordDictionary[7] = word
+        if len(word) == 4:
+            wordDictionary[4] = word
+        if len(word) == 7:
+            wordDictionary[8] = word
     curTotal = 0
     for word in code.split():
-        curTotal = curTotal * 10 + getNumberFromWord(word, d)
+        curTotal = curTotal * 10 + getNumberFromWord(word, wordDictionary)
         if len(word) in [2,3,4,7]:
             count += 1
     total += curTotal
